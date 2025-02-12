8bb3efb510b55a55d7edf7d054b7584946d0fa43
Navin Sah <nabinsah703@gmail.com>
2025-02-12 22:17:51 +0545

    configure connection from sql using ado.net

---
+++ Connection/SqlConnectionFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace HotelManagementSystem.Connection
+{
+    public class SqlConnectionFactory
+    {
+        private readonly IConfiguration _configuration;
+        public SqlConnectionFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public SqlConnection CreateConnection() =>
+            new SqlConnection(_configuration.GetConnectionString("DefaultConnection"));
+    }
+}
