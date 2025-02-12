67199da2552176964a9db7118c63dba9aff7e626
Navin Sah <nabinsah703@gmail.com>
2025-02-12 22:53:15 +0545

    create user related all DTOs classes to handle user

---
+++ DTOs/UserDTOs/LoginUserResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelManagementSystem.DTOs.UserDTOs
+{
+    public class LoginUserResponseDTO
+    {
+        public int UserId { get; set; }
+        public string Message { get; set; }
+        public bool IsLogin { get; set; }
+    }
+}
